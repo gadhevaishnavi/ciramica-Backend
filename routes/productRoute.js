f29abb356979b87411453b4bacc13a6d550ec054
vaishnavi gadhe <gadhevaishnavi70@gmail.com>
2025-03-05 15:16:56 +0530

    update code

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,30 +1,3 @@
-// import express from 'express';
-// import {
-//     createProductController,
-//     getAllProductsController,
-//     getProductByReferenceController,
-//     updateProductByReferenceController,
-//     deleteProductByReferenceController,
-//     getProductsByBrandController,
-//     getProductsInStockController,
-//     searchProductsController,
-// } from '../controllers/productController.js';
-
-// const productRoute = express.Router();
-
-
-// productRoute.post('/products', createProduct);
-// productRoute.get('/products', getAllProductsController);
-// productRoute.get("/products/:id", productController.getProductById);
-// productRoute.get('/products/:reference', getProductByReferenceController);
-// productRoute.put('/products/:reference', updateProductByReferenceController);
-// productRoute.delete('/products/:reference', deleteProductByReferenceController);
-// productRoute.get('/products/brand/:brand', getProductsByBrandController);
-// productRoute.get('/products/in-stock', getProductsInStockController);
-// productRoute.get('/products/search', searchProductsController);
-
-// export default productRoute;
-
 import express from "express";
 import * as productController from "../controllers/productController.js";
 
